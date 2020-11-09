2a0e742913e193c263e9b63675cbd4d588102c4a
Saikat Roy <njmsaikat@gmail.com>
2020-11-10 01:04:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 
-//movement
-const card = document.querySelector('.card');
-const container = document.querySelector('.container');
-
-//moving animation event
-container.addEventListener('mousemove', (e) => {
-    
-})
